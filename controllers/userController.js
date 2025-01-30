393246aa89b7dadfe18752b81cce139c934850f4
Abhishek Dey <71166991+Abhishek20110@users.noreply.github.com>
2025-01-30 15:00:31 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,8 +47,8 @@ export const loginUser = async (req, res) => {
 
 
         // Generate JWT token
-     //   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '15s' });
+       const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+      //  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '15s' });
 
 
         res.status(200).json({
